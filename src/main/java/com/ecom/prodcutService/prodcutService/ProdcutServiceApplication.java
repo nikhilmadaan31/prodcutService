4bc4f0b66a238c44a8fd05f16b6ca02d60bd3340
Nikhil Madaan <nikhilmadaan29@gmail.com>
2024-10-06 14:42:57 +0530

    Initial commit for product-service

---
+++ src/main/java/com/ecom/prodcutService/prodcutService/ProdcutServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.prodcutService.prodcutService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdcutServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdcutServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecom/prodcutService/prodcutService/ProdcutServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.prodcutService.prodcutService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdcutServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
